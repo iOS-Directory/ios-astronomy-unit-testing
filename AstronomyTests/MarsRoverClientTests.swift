5f6053d899d8a264e3e39abfffaaf999c9d7d497
fritzgt <tuanis@live.com>
2020-04-07 14:53:41 +0300

    initial commit

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,22 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by FGT MAC on 4/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+/*
+ 1.Test is the data is coming back with the expected info
+ 2.Check errors are being handle for invalid JSON
+ 3.Check errors are being handle for unspected info
+ 4.Check errors are being handle for timeout
+*/
+
+class MarsRoverClientTests: XCTestCase {
+
+
+
+}
